90913729677d224a24d0d9a73ade4a610dda7e7b
tyler-Wiz <tylertooxclusive@gmail.com>
2023-12-25 09:58:40 +0000

    [frontend]feat: Redux slice for Register and Login user

---
+++ frontend/src/services/config/index.js
@@ -0,0 +1,11 @@
+const REGISTER_URL = "http://localhost:4002/auth/register";
+const LOGIN_API_URL = "http://localhost:4002/auth/login";
+const FORGOT_PASSWORD_URL = "http://localhost:4002/auth/forgot";
+const RESET_PASSWORD_URL = "http://localhost:4002/auth/reset/";
+
+export default {
+  REGISTER_URL,
+  LOGIN_API_URL,
+  FORGOT_PASSWORD_URL,
+  RESET_PASSWORD_URL,
+};
---
+++ frontend/src/store/features/authSlice.js
@@ -0,0 +1,124 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import Cookies from "universal-cookie";
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
+import config from "@/services/config/index";
+
+const cookie = new Cookies();
+
+const initialState = {
+  token: cookie.get("session") || "",
+  id: "",
+  email: "",
+  password: "",
+  created_at: "",
+  is_admin: false,
+  registerStatus: "",
+  registerError: "",
+  loginStatus: "",
+  loginError: "",
+};
+
+// Register new user
+export const registerUser = createAsyncThunk(
+  "auth/registerUser",
+  async (user, { rejectWithValue }) => {
+    try {
+      const token = await axios.post(`${config.REGISTER_URL}`, user, {
+        withCredentials: true,
+      });
+      cookie.set("session", token.data, {
+        maxAge: 7 * 24 * 3600,
+      });
+      return token.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+// Login user into state
+export const loginUser = createAsyncThunk(
+  "auth/loginUser",
+  async (user, { rejectWithValue }) => {
+    try {
+      const token = await axios.post(`${config.LOGIN_API_URL}`, user, {
+        withCredentials: true,
+      });
+      cookie.set("session", token.data, {
+        maxAge: 7 * 24 * 3600,
+      });
+      return token.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    getUser(state) {
+      const token = state.token;
+      if (token) {
+        const user = jwtDecode(token);
+        return {
+          ...state,
+          id: user.id,
+          email: user.email,
+          password: user.password,
+          created_at: user.created_at,
+          is_admin: user.is_admin,
+          userLoaded: true,
+        };
+      }
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(registerUser.pending, (state) => {
+      return { ...state, registerStatus: "pending" };
+    });
+    builder.addCase(registerUser.fulfilled, (state, action) => {
+      if (action.payload) {
+        return {
+          ...state,
+          token: action.payload,
+          registerStatus: "success",
+          registerError: "",
+        };
+      } else return state;
+    });
+    // eslint-disable-next-line no-unused-vars
+    builder.addCase(registerUser.rejected, (state, action) => {
+      return {
+        ...state,
+        registerStatus: "rejected",
+        registerError: action.payload,
+      };
+    });
+    builder.addCase(loginUser.pending, (state) => {
+      return { ...state, loginStatus: "pending" };
+    });
+    builder.addCase(loginUser.fulfilled, (state, { payload }) => {
+      if (payload) {
+        return {
+          ...state,
+          token: payload,
+          loginError: "",
+          loginStatus: "",
+        };
+      } else return state;
+    });
+    builder.addCase(loginUser.rejected, (state, { payload }) => {
+      return {
+        ...state,
+        loginStatus: "rejected",
+        loginError: payload,
+      };
+    });
+  },
+});
+
+export const { getUser } = authSlice.actions;
+export default authSlice.reducer;
