85418eaf77821d8a5b9760ca06714b367903c25d
tyler-Wiz <tylertooxclusive@gmail.com>
2024-01-26 12:01:17 +0000

    [frontend]styling :footer styling

--- frontend/src/components/client/Footer.jsx
+++ frontend/src/components/client/Footer.jsx
@@ -6,7 +6,7 @@ import { MdEmail } from "react-icons/md";
 const Footer = () => {
   return (
     <footer className="montserrat py-10 mt-10 px-6 border-t-[1px]">
-      <div className="lg:container">
+      <div className="lg:container gap-10">
         <div className="lg:w-1/5 flex flex-col gap-2">
           <Image
             src="/img/client/logo.png"
@@ -27,7 +27,7 @@ const Footer = () => {
             <p>hello@sportzystore.com</p>
           </div>
         </div>
-        <div className="lg:flex-item lg:justify-between lg:w-2/5 mt-8">
+        <div className="lg:flex-item lg:justify-between lg:w-4/5 mt-8 lg:mt-0">
           {FooterData.map((item, index) => (
             <div key={index}>
               <h3 className="font-bold text-lg mb-4">{item.header}</h3>
@@ -38,22 +38,25 @@ const Footer = () => {
               ))}
             </div>
           ))}
-        </div>
-        <div className="lg:w-1/5">
-          <h3 className="font-bold text-lg">NewsLetter</h3>
-          <form action="" className="flex-item my-6">
-            <input placeholder="Enter your email address.." />
-            <button className=" bg-red-700 py-3 px-3 border border-gray-300">
-              <FaArrowRightLong color="white" />
-            </button>
-          </form>
           <div>
-            <Image
-              src="/img/client/payments.png"
-              width={300}
-              height={50}
-              className="mt-6"
-            />
+            <h3 className="font-bold text-lg w-2/5">NewsLetter</h3>
+            <form action="" className="flex-item my-6">
+              <input
+                placeholder="Enter your email address.."
+                className="px-2 py-2 placeholder:text-sm border border-gray-300 w-3/5 outline-none"
+              />
+              <button className=" bg-red-700 py-3 px-3 border border-gray-300">
+                <FaArrowRightLong color="white" />
+              </button>
+            </form>
+            <div>
+              <Image
+                src="/img/client/payments.png"
+                width={300}
+                height={50}
+                className="mt-6"
+              />
+            </div>
           </div>
         </div>
       </div>
